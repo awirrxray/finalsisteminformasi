b612f99d59a49b14917cf781d9aeab2503a01e4a
Muhammad Musyawir <awirrxray@gmail.com>
2018-12-21 11:25:16 +0800

    Final Sistem Informasi

---
+++ application/views/v_input.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</title>
+</head>
+<body>
+	<center>
+		<h1>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</h1>
+		<h3>Tambah data baru</h3>
+	</center>
+	<form action="<?php echo base_url(). 'crud/tambah_aksi'; ?>" method="post">
+		<table style="margin:20px auto;">
+			<tr>
+				<td>Nama Perusahaan</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>No. Telp Perusahaan</td>
+				<td><input type="text" name="notelp"></td>
+			</tr>
+			<tr>
+				<td>Email perusahaan</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td>Alamat Perusahaan</td>
+				<td><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Tambah"></td>
+			</tr>
+		</table>
+	</form>	
+</body>
+</html>
---
+++ company.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 21, 2018 at 02:09 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `notelp` int(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
